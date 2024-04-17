f9e677b147ace1697a2bf6b0e8881228b94a4c86
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-17 15:58:16 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -8,8 +8,24 @@ const HomePage = () => {
                 <h1 className="text-3xl font-semibold">Sri Lankan Railways</h1>
             </div>
         </div>
-        <div className="bg-green-500 lg:mx-24 mx-8 py-16 px-12 mt-[-100px] rounded">
-            Book a Tickent
+        <div className="bg-white lg:mx-24 mx-8 py-16 px-12 mt-[-100px] rounded shadow-2xl">
+            <form>
+                <div className="lg:flex">
+                    <div className="">
+                        <label htmlFor="from" >From</label><br />
+                        <select id="countries" class="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Choose a Start Location</option>
+                            <option value="US">United States</option>
+                            <option value="CA">Canada</option>
+                            <option value="FR">France</option>
+                            <option value="DE">Germany</option>
+                        </select>
+                    </div>
+                    <div className="">
+                        
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
   )
