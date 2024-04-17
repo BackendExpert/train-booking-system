0a273a2833b7bc6a72e3e231e091023e72154e54
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-17 19:14:26 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -40,7 +40,7 @@ const HomePage = () => {
                     </div>
                     <div className="w-full">
                         <label htmlFor="from" >To</label><br />
-                        <input type="date" name="" id="" />
+                        <input type="date" name="" id="" className='my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'/>
                     </div>
                 </div>
             </form>
