5fe77abacf13ca7209ce6947c2a0c69ba0e35983
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-17 15:45:44 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='lg:py-[4%] lg:px-16 bg-[url(https://wallpapercave.com/wp/wp12713262.jpg)] bg-center bg-cover lg:h-[80vh] h-screen w-full'>
-        <div className="">
-            <h1 className="">Railways</h1>
+    <div className='lg:py-[4%] lg:px-16 bg-[url(https://wallpapercave.com/wp/wp12713228.jpg)] bg-center bg-cover lg:h-[80vh] h-screen w-full'>
+        <div className="text-white text-center my-12">
+            <h1 className="text-3xl font-semibold">Sri Lankan Railways</h1>
         </div>
     </div>
   )
