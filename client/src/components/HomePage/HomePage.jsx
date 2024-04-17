567e330ad88dbe3725c909283869fb01c2fce87e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-17 15:36:33 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='lg:py-[4%] lg:px-16 bg-[url(https://wallpapercave.com/wp/OHd7y41.jpg)] bg-center bg-cover h-96 w-full'>
+        Home Page
+    </div>
   )
 }
 
