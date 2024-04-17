c0171a9cee50ce21a83ec0d1e7bb6770f395cb46
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-17 16:02:01 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -22,8 +22,18 @@ const HomePage = () => {
                             <option value="DE">Germany</option>
                         </select>
                     </div>
-                    <div className="">
+                    <div className="mx-12 my-4">
                         <MyIcons name='arrow-forward' size='large'></MyIcons>
+                    </div>
+                    <div className="">
+                        <label htmlFor="from" >To</label><br />
+                        <select required className="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Choose a </option>
+                            <option value="US">United States</option>
+                            <option value="CA">Canada</option>
+                            <option value="FR">France</option>
+                            <option value="DE">Germany</option>
+                        </select>
                     </div>
                 </div>
             </form>
