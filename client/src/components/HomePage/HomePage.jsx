2e07aaa0781dbcd3316b3cab2daf9e4aff6135eb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-17 16:01:08 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
                 <div className="lg:flex">
                     <div className="">
                         <label htmlFor="from" >From</label><br />
-                        <select id="countries" class="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <select required className="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option selected>Choose a Start Location</option>
                             <option value="US">United States</option>
                             <option value="CA">Canada</option>
