1c8992d5bb23949e5c2bdd58c16f841068112b5d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 15:44:30 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -10,12 +10,23 @@ const SignUp = () => {
         <div className='mx-20'>
             <div className="lg:flex bg-white shadow-md rounded w-full h-auto">
                 <div className="">
-                    <img src="https://wallpapercave.com/wp/wp12713228.jpg" alt="" className='rounded' />
+                    <img src="https://wallpapercave.com/wp/wp12713228.jpg" alt="" className='rounded h-full' />
                 </div>
-                <div className="mx-5 py-12 px-10">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad earum facilis saepe? Assumenda nulla modi, cum doloremque cupiditate omnis hic perferendis dolorem dolores sapiente tempore unde nam praesentium consectetur est!
-                    Reprehenderit aut et accusantium. Veniam ipsum eaque asperiores cum, voluptatum recusandae! Dolor minus deleniti eos mollitia tempora, molestias deserunt tempore esse quasi ratione repellat maxime dolores eum, harum itaque. Sed.
-
+                <div className="mx-5 py-12 px-10 w-full">
+                    <form className=''>
+                        <div className="my-5">
+                            <label htmlFor="">Username : </label>
+                            <input type="text" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4' required placeholder='Enter Username'/>
+                        </div>
+                        <div className="my-5">
+                            <label htmlFor="">Email : </label>
+                            <input type="email" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4' required placeholder='Enter Email'/>
+                        </div>
+                        <div className="my-5">
+                            <label htmlFor="">Password : </label>
+                            <input type="password" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4' required placeholder='Enter Password'/>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
