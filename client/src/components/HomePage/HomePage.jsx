ca22dc1312a8c26dfd80dbfad4ef4b06b5aaf80f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 15:17:58 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -18,6 +18,9 @@ const HomePage = () => {
         <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp12713228.jpg)] bg-center bg-cover lg:h-[80vh] h-screen w-full'>
             <div className="text-white text-center my-12">
                 <h1 className="text-3xl font-semibold">Sri Lankan Railways</h1>
+                <div className="my-16">
+                    Be a Member
+                </div>
             </div>
         </div>
         <div className="bg-white lg:mx-24 mx-8 py-16 px-12 lg:mt-[-100px] mt-[-280px] rounded shadow-2xl lg:mb-40 mb-20">
