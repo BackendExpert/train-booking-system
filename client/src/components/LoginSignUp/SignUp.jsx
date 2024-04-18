4f58d3c302ac3d26cf314b640bbf2ee6d8922a34
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 15:38:30 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -6,10 +6,13 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
-        <div className='my-24 mx-20'>
-            <div className="flex">
-                <div className="bg-blue-500 rounded w-full h-auto py-12 px-10">
+    <div className='bg-gray-200 py-24'>
+        <div className='mx-20'>
+            <div className="lg:flex bg-white shadow-md rounded  w-full h-auto py-12 px-10">
+                <div className="mx-5">
+                    <img src="" alt="" />
+                </div>
+                <div className="mx-5">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad earum facilis saepe? Assumenda nulla modi, cum doloremque cupiditate omnis hic perferendis dolorem dolores sapiente tempore unde nam praesentium consectetur est!
                     Reprehenderit aut et accusantium. Veniam ipsum eaque asperiores cum, voluptatum recusandae! Dolor minus deleniti eos mollitia tempora, molestias deserunt tempore esse quasi ratione repellat maxime dolores eum, harum itaque. Sed.
 
