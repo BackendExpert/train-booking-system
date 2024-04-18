aeb017a90bf1b798427a10cfa8fedb2988fba237
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 09:04:29 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -26,9 +26,9 @@ const HomePage = () => {
                         <MyIcons name='arrow-forward' size='large'></MyIcons>
                     </div>
                     <div className="">
-                        <label htmlFor="from" >To</label><br />
+                        <label htmlFor="from" >Destination</label><br />
                         <select required className="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected>Choose a </option>
+                            <option selected>Choose a Destination</option>
                             <option value="US">United States</option>
                             <option value="CA">Canada</option>
                             <option value="FR">France</option>
