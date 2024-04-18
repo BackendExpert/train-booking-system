f21567b40a1e863abb48897be07180f0664b340f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 15:33:09 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const SignUp = () => {
   return (
     <div>
-        <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp12713228.jpg)] bg-center bg-cover h-screen w-full'>
+        <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp5682561.jpg)] bg-center bg-cover h-screen w-full'>
             Hi all
         </div>
     </div>
