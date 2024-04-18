df78169050920d8f90011f3f42c2c88ec89cad4b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 15:32:01 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState } from 'react'
+import MyIcons from '@reacticons/ionicons'
+import { Link } from "react-router-dom";
 
 // https://wallpapercave.com/wp/wp5682561.jpg
 
