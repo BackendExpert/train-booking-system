243b8c9b64cd6ba7b28ff7b99f9939edc6d4077a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 09:30:57 +0530

    Update History.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -4,7 +4,7 @@ import Footer from '../HomePage/Footer'
 const History = () => {
   return (
     <div className="">
-        <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp10586122.jpg)] bg-center bg-cover lg:h-[50vh] h-screen w-full'>
+        <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp9872826.jpg)] bg-center bg-cover lg:h-[50vh] h-screen w-full'>
             <h1 className="text-white text-center font-semibold text-3xl lg:my-20">History</h1>
         </div>
 
