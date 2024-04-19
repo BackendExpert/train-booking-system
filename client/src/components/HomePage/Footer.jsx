f6d8be5ca992642d9aef511e069921d7a4f0f1dd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 10:09:11 +0530

    Update Footer.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/Footer.jsx
+++ client/src/components/HomePage/Footer.jsx
@@ -46,9 +46,18 @@ const Footer = () => {
                 <h1 className="text-xl font-semibold font-semibold text-gray-500 my-4 ">Contact</h1>
                 <p className="my-5">jehan@123.com</p>   
                 <p className="my-5">+94b 711758851</p>  
-                <p className="my-5">88, ABC Street, XYZ</p>  
+                <p className="my-5">88, ABC Street, XYZ</p> 
+
+                <h1 className='mt-8 font-semibold'></h1>
+                <div className="flex">
+                    <form>
+                        <input type="text" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4' required placeholder='Email Address'/>
+                        <button type="submit" className='bg-blue-500 text-white shadow-md rounded '>Subscribe for latest Update</button>
+                    </form>
+                </div> 
             </div>
         </div>
+
         <hr className='border-t-2 border-gray-200'/>
         <div className="text-center my-2">
             &copy; The Train Booking System - 2024 || Developed and Designed by : Jehankandy
