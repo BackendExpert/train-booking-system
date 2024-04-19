5beb9aff8c14b4dc9a31b6822446dd3f0c5815fe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 09:34:01 +0530

    Update History.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -7,6 +7,9 @@ const History = () => {
         <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp9872826.jpg)] bg-center bg-cover lg:h-[50vh] h-screen w-full'>
             <h1 className="text-white text-center font-semibold text-3xl lg:my-20">History</h1>
         </div>
+        <div className="bg-gray-200">
+            <h1 className="text-center text-4xl font-semibold text-gray-500 mb-4">History of the Railways</h1>
+        </div>
 
         <Footer />
     </div>
