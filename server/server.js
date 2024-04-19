5bc2abd0e1ff2b8cb7e4c655bd2dedd0c25ff4eb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 12:04:56 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1234",
-    database: "db_erp"
+    database: "db_tbs"
 })
 //email Sending - Nodemailer transporter
 
