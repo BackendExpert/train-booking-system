474c4cb9c6ca707fcc7474fc2ad0bb95db56f5aa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 10:26:48 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const path = require('path')
+
+const resourceLimits = require('worker_threads');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 8081
+
+//file  upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images')
+    }, 
+    filename:(req, file, cb) => {
+      cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    }
+})
+  
+const upload = multer({
+    storage:storage
+})
+
+
+//make connection between dbsever and node app
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "db_erp"
+})
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public')); 
+
+
+//check the server is working
+app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
