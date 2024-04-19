47200d26f173fe0efaced4ffd31bf81095641a3e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 09:42:24 +0530

    Update NavBar.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/NavBar.jsx
+++ client/src/components/HomePage/NavBar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import MyIcons from '@reacticons/ionicons'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='mb-20'>
+        <div className="bg-none text-white mt-[-20px]">
+            <div className="flex justify-between mx-16">
+                <div className="flex">
+                    <MyIcons name='train' size='large' ></MyIcons>
+                    <h1 className="my-1">Train Booking</h1>
+                </div>
+                <div className="my-1">
+                    login
+                </div>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
