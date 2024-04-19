5d846e0eae994a5eeb27dbae9e1a63db01ff07ed
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 11:06:42 +0530

    Uopdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -1,10 +1,18 @@
 import React, {useState } from 'react'
 import MyIcons from '@reacticons/ionicons'
 import { Link } from "react-router-dom";
+import secureLocalStorage from "react-secure-storage"
+
 
 // https://wallpapercave.com/wp/wp5682561.jpg
 
 const SignUp = () => {
+    const [UserData, SetUserData] = useState({
+        username: '',
+        email:'',
+        password: '',
+    })
+
   return (
     <div className='bg-gray-200 py-24'>
         <div className='lg:mx-20 mx-8'>
