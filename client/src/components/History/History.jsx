797f64a81592a7096ed317798035ba0bc1530a30
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 09:24:10 +0530

    Update History.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const History = () => {
   return (
-    <div>History</div>
+    <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp12713228.jpg)] bg-center bg-cover lg:h-[50vh] h-screen w-full'>
+        <h1 className="text-white text-center font-semibold text-3xl lg:my-20">History</h1>
+    </div>
   )
 }
 
