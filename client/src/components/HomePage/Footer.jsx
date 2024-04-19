a343a5bc08bbb86bffb559d7363b9b4150dc31af
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 09:25:54 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/Footer.jsx
+++ client/src/components/HomePage/Footer.jsx
@@ -2,7 +2,47 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-white px-16 py-12'>
+        <div className="lg:flex">
+            <div className="lg:px-4 px-0 w-full">
+                <h1 className="text-xl font-semibold font-semibold text-gray-500 my-4 ">About us</h1>
+                <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate iste suscipit nesciunt, dolore inventore provident fuga incidunt quibusdam nobis tenetur culpa at! Facilis, rerum illo? Pariatur aperiam error corporis fuga.</p>
+            </div>
+            <div className="lg:px-4 px-0 w-full">
+                <h1 className="text-xl font-semibold font-semibold text-gray-500 my-4 ">Our Services</h1>
+                <div className="">
+                    <p className="my-5">Online Ticket Booking</p>   
+                    <p className="my-5">Passenger Transportation</p>  
+                    <p className="my-5">Cargo Transportation</p>  
+                </div>
+                
+            </div>
+            <div className="lg:px-4 px-0 w-full">
+                <h1 className="text-xl font-semibold font-semibold text-gray-500 my-4 ">More About</h1>
+                <div className="">
+                {
+                    footerData.map((data) => {
+                        return (
+                            <Link to={data.link}>
+                                <p className="my-5">{data.name}</p>
+                            </Link>
+                        )
+                    })
+                }
+                </div>
+            </div>
+            <div className="lg:px-4 px-0 w-full">
+                <h1 className="text-xl font-semibold font-semibold text-gray-500 my-4 ">Contact</h1>
+                <p className="my-5">jehan@123.com</p>   
+                <p className="my-5">+94b 711758851</p>  
+                <p className="my-5">88, ABC Street, XYZ</p>  
+            </div>
+        </div>
+        <hr className='border-t-2 border-gray-200'/>
+        <div className="text-center my-2">
+            &copy; The Train Booking System - 2024 || Developed and Designed by : Jehankandy
+        </div>
+    </footer>
   )
 }
 
