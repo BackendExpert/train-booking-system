dbf447081ba45fab8383b333b6e6f1743569106b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 15:06:33 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignIn.jsx
+++ client/src/components/LoginSignUp/SignIn.jsx
@@ -19,11 +19,11 @@ const SignUp = () => {
                     <form className=''>
                         <div className="my-5">
                             <label htmlFor="">Email : </label>
-                            <input type="email" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4' required placeholder='Enter Email'/>
+                            <input type="email" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Email'/>
                         </div>
                         <div className="my-5">
                             <label htmlFor="">Password : </label>
-                            <input type="password" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4' required placeholder='Enter Password'/>
+                            <input type="password" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Password'/>
                         </div>
                         <div className="">
                             <button type='submit' className='w-full bg-blue-500 rounded py-4 px-8 text-white duration-500 hover:bg-blue-600 hover:shadow-md'>SignIn</button>
