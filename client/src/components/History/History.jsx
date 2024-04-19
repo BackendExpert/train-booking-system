233078dc9afe3f65dff43ad7734fe2bffab2ee34
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-19 09:38:02 +0530

    Update History.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -5,10 +5,20 @@ const History = () => {
   return (
     <div className="">
         <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp9872826.jpg)] bg-center bg-cover lg:h-[50vh] h-screen w-full'>
-            <h1 className="text-white text-center font-semibold text-3xl lg:my-20">History</h1>
+            <h1 className="text-white text-center font-semibold text-3xl lg:my-20 my-40">History</h1>
         </div>
-        <div className="bg-gray-200">
-            <h1 className="text-center text-4xl font-semibold text-gray-500 mb-4">History of the Railways</h1>
+        <div className="bg-gray-200 py-16">
+            <h1 className="text-center text-2xl font-semibold text-gray-500 mb-4">History of the Railways</h1>
+            <p className="lg:mx-16 mx-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat fuga blanditiis minima suscipit minus recusandae magni perferendis hic asperiores expedita, dolorum nostrum? Asperiores, repellat? Tempore in sint iusto soluta est?
+                Porro sit, harum repellendus alias vero recusandae laudantium voluptas voluptatem sunt quae minima minus. Fugit error, accusantium quae odio exercitationem praesentium eaque quam, ad nobis deserunt atque inventore quas eveniet.
+                Nemo laborum maxime corporis exercitationem inventore eius praesentium hic fugiat velit! Deserunt, dignissimos necessitatibus porro impedit, optio provident quaerat non, voluptate dolorum iure saepe id exercitationem illo. Nisi, illo ut!
+                Sint est velit rem vero, ducimus dicta soluta ullam! Distinctio dolorum, laborum debitis architecto esse sint hic tenetur nemo ea quisquam illum natus, ad accusamus impedit delectus temporibus quasi officiis.
+                Quis, ducimus fugit deserunt, autem ipsum consequuntur fuga voluptate ut doloribus iusto cupiditate, numquam dicta laudantium sunt. Est sequi, similique accusamus laboriosam iure illum quam perspiciatis totam cum modi laborum.
+                Ex, perspiciatis vitae reprehenderit nulla dolores ipsam cum adipisci neque quasi, numquam quidem repellendus. Itaque, mollitia totam voluptatibus autem ratione sed maiores quidem ducimus ullam culpa iste, suscipit aut voluptate!
+                Culpa voluptate tempora earum ullam repellendus voluptates quidem labore officia impedit fuga accusamus blanditiis dolores deleniti nemo odio neque architecto asperiores deserunt numquam expedita, aliquid commodi laudantium? Quos, obcaecati maiores.
+                Dicta in ut assumenda omnis vel dolorem earum non. Repellat placeat ea esse officia nihil incidunt iure consequatur dolorem praesentium sapiente, illo iste explicabo aliquam qui possimus eum est ullam?
+            </p>
         </div>
 
         <Footer />
